6e96c3bb9679ff917b0ebfb80ceac74b5223b62e
jenishgajera780 <136606761+jenishgajera780@users.noreply.github.com>
2023-07-03 23:54:18 +0530

    Update PR_3_3.C

--- PR_3_3.C
+++ PR_3_3.C
@@ -1,1 +1,18 @@
 // PR Q3 write a program to find the sum of a first and last gigit of a number.
+#include<stdio.h>
+#include<conio.h>
+void main(){
+int j,e,n,i;
+
+printf("type any number : ");
+scanf("%d",&j);
+n = j%10;
+while(j>10){
+e = j/10;
+j = e;{
+i=e+n;
+printf("the number of first and last :",i);}
+    
+}
+getch();
+}
